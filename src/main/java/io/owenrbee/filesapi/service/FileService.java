d68188550717db188fe72f2cb4f7f94596bf70d2
Owen Bringino <owenrbee@gmail.com>
2025-02-22 22:40:52 +0800

    Add caching library

--- src/main/java/io/owenrbee/filesapi/service/FileService.java
+++ src/main/java/io/owenrbee/filesapi/service/FileService.java
@@ -2,6 +2,7 @@ package io.owenrbee.filesapi.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import io.owenrbee.filesapi.model.FileVO;
@@ -14,11 +15,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public final class FileService {
+public class FileService {
 
     private static final Logger logger = LoggerFactory.getLogger(FileService.class);
 
+    @Cacheable("files")
     public List<FileVO> getFiles(String directory) {
+
+        logger.info("reading files from: " + directory);
+
         List<FileVO> filesAndDirectories = new ArrayList<>();
 
         // Create a File object representing the specified directory
