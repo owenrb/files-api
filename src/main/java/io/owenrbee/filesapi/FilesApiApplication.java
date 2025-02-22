6305b751ec471c14736acfd684f3cdd7d5a3c724
Owen Bringino <owenrbee@gmail.com>
2025-02-22 21:27:08 +0800

    Spring Initializr

---
+++ src/main/java/io/owenrbee/filesapi/FilesApiApplication.java
@@ -0,0 +1,13 @@
+package io.owenrbee.filesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/owenrbee/filesapi/FilesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.owenrbee.filesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
